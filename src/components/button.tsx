18c2fc6b7f48362c3fc86ce0c7e2e8af121a4811
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-08-12 18:43:13 -0300

    feat: instala e configura tailwind-config-prettier para ordenar as classes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,8 @@
 export function Button() {
-  return <button className="bg-amber-900">botão</button>;
+  return (
+    <button className="bg-amber-900">
+      botão
+      <div className="flex items-center justify-between rounded-md bg-red-500 p-4 text-white">Teste</div>
+    </button>
+  );
 }
