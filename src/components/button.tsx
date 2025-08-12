ae0ccb66c434010d761862a777707acff9847543
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-08-12 19:15:59 -0300

    feat: configuracao inicial dos tokens

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ export function Button() {
   return (
     <button className="bg-amber-900">
       botão
-      <div className="flex items-center justify-between rounded-md bg-red-500 p-4 text-white">Teste</div>
+      <div className="bg-red-500 flex items-center justify-between rounded-md p-4 text-violet-700">Teste</div>
     </button>
   );
 }
