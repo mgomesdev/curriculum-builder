d82bdee95581ccf5e991eec2b5a4a42ff4fcdcaf
Matheus Gomes <matheusgomes1203@hotmail.com>
2025-08-12 19:17:40 -0300

    Feat/matheusgomes/27 configurar o tailwind (#48)
    * feat: instala tailwind
    
    * feat: instala e configura tailwind-config-prettier para ordenar as classes
    
    * feat: configuracao inicial dos tokens

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,8 @@
 export function Button() {
-  return <button>botão</button>;
+  return (
+    <button className="bg-amber-900">
+      botão
+      <div className="bg-red-500 flex items-center justify-between rounded-md p-4 text-violet-700">Teste</div>
+    </button>
+  );
 }
