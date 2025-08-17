ff1e48a17451f1e06809a4b96945a62b71a2a82d
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-08-17 11:59:14 -0300

    feat: estiliza o template

--- src/features/template/editor/editor.tsx
+++ src/features/template/editor/editor.tsx
@@ -1,39 +1,38 @@
-import React, { useRef } from 'react';
-import { fakeTemplateApiResponse, templateBuilderData } from '../builder/template-builder-data';
+import { useRef } from 'react';
+import { templateBuilderData } from '../builder/template-builder-data';
 import { TemplateBuilder } from '../builder/template-builder';
-import { domToJSON, renderFromJSON } from '../utils/domToJSON';
+/*import { domToJSON, renderFromJSON } from '../utils/domToJSON';
 import type { Template } from '../dto/template-dto';
-
+*/
 export const TemplateEditor = () => {
   const divRef = useRef<HTMLDivElement>(null);
-
+  /*
   const handleDomToJSON = () => {
     if (divRef?.current) {
       const json = domToJSON(divRef.current);
       console.log(json);
     }
-  };
+  }; */
 
   return (
     <>
-      <button onClick={handleDomToJSON}>domTOJson</button>
       <TemplateBuilder initialData={templateBuilderData} ref={divRef} />
+      {/*
 
       <hr />
+      <button onClick={handleDomToJSON}>domTOJson</button>
 
       <RenderFromJSON template={fakeTemplateApiResponse} />
+*/}
     </>
   );
 };
-
-const RenderFromJSON = ({ template }: { template: Template }) => {
-  if (!template) return null;
-
-  return (
-    <>
-      {template.page.children.map((child, index) => (
-        <React.Fragment key={index}>{renderFromJSON(child)}</React.Fragment>
-      ))}
-    </>
-  );
-};
+/*
+const RenderFromJSON = ({ template }: { template: Template }) => (
+  <>
+    {template.page.children.map((child, index) => (
+      <React.Fragment key={index}>{renderFromJSON(child)}</React.Fragment>
+    ))}
+  </>
+);
+*/
