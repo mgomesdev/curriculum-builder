dc65a7a70878d06d2ce952a26d652b87e264a9dc
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-08-24 19:30:49 -0300

    refactor: descomenta codigo e faz sortable funcionar

--- src/features/template/editor/editor.tsx
+++ src/features/template/editor/editor.tsx
@@ -1,6 +1,9 @@
-import React, { createContext, use, useRef, useState } from 'react';
+import React, { createContext, forwardRef, use, useRef, useState, type ComponentProps } from 'react';
 import { fakeTemplateApiResponse } from '../builder/template-builder-data';
-import { domToJSON, renderFromJSON } from '@/utils/template-utils';
+import { domToJSON } from '@/utils/template-utils';
+import { closestCenter, DndContext, PointerSensor, useSensor, useSensors, type DragEndEvent } from '@dnd-kit/core';
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 export const TemplateEditor = () => {
   return (
@@ -72,78 +75,41 @@ const TemplateEditorContent = () => {
         )}
       </header>
 
-      {fakeTemplateApiResponse.page.children.map((child, index) => (
+      {/*fakeTemplateApiResponse.page.children.map((child, index) => (
         <React.Fragment key={index}>{renderFromJSON(child)}</React.Fragment>
-      ))}
+      ))*/}
+
+      <SortableElements />
     </>
   );
 };
 
-/*
-interface ItemProps {
-  node: PageNode;
-  path: number[]; // caminho para identificar a posição na árvore
-}
-
-const SortableItem = ({ node, path }: ItemProps) => {
-  const id = path.join('-'); // cria um identificador único a partir do caminho
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-    border: '1px dashed #aaa',
-    padding: 8,
-    marginBottom: 4,
-  };
-
-  return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <span>{node.type}</span>
-      {node.children && (
-        <div style={{ paddingLeft: 16 }}>
-          <SortableContext
-            items={node.children.map((_, i) => [...path, i].join('-'))}
-            strategy={verticalListSortingStrategy}>
-            {node.children.map((child, i) =>
-              typeof child === 'string' ? (
-                <div key={i}>{child}</div>
-              ) : (
-                <SortableItem key={i} node={child} path={[...path, i]} />
-              )
-            )}
-          </SortableContext>
-        </div>
-      )}
-    </div>
-  );
-};
-
 const SortableElements = () => {
-  const headers = fakeTemplateApiResponse.page.children[0].children.filter(n => n.type === 'header');
-  const [items, setItems] = useState(headers);
+  const [items, setItems] = useState(['1', '2']);
 
   const sensors = useSensors(useSensor(PointerSensor));
 
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
     if (!over || active.id === over.id) return;
-    const oldIndex = items.findIndex(i => i.type + i.props.id === active.id);
-    const newIndex = items.findIndex(i => i.type + i.props.id === over.id);
+    const oldIndex = items.findIndex(i => i === active.id);
+    const newIndex = items.findIndex(i => i === over.id);
     setItems(arrayMove(items, oldIndex, newIndex));
   };
 
   return (
     <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-      <SortableContext items={items.map(i => i.type + i.props.id)} strategy={verticalListSortingStrategy}>
-        {items.map((item, index) => (
-          <SortableItem key={index} node={item} path={[index]} />
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {items.map(item => (
+          <SortableItem id={item} key={item}>
+            {item}
+          </SortableItem>
         ))}
       </SortableContext>
     </DndContext>
   );
 };
-/*
+
 interface SortableItemProps {
   children: React.ReactNode;
   id: string;
@@ -157,7 +123,6 @@ const SortableItem = ({ children, id }: SortableItemProps) => {
     transition,
   };
 
-  console.log(id);
   return (
     <Item ref={setNodeRef} style={style} {...attributes} {...listeners}>
       {children}
@@ -169,16 +134,13 @@ interface ItemProps extends ComponentProps<'div'> {
   children?: React.ReactNode;
 }
 
-const Item = forwardRef<HTMLDivElement, ItemProps>(({ children, ...props }, ref) => {
-  return (
-    <div
-      {...props}
-      ref={ref}
-      className="cursor-move rounded-md border border-dashed border-gray-300 p-4 hover:border-blue-400">
-      {children}
-    </div>
-  );
-});
+const Item = forwardRef<HTMLDivElement, ItemProps>(({ children, ...props }, ref) => (
+  <div
+    {...props}
+    ref={ref}
+    className="cursor-move rounded-md border border-dashed border-gray-300 p-4 hover:border-blue-400">
+    {children}
+  </div>
+));
 
 Item.displayName = 'Item';
-*/
