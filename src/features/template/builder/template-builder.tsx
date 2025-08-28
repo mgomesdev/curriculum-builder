90100eb89f0d6d0161889e83311d5b82dc401670
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2025-08-27 21:51:06 -0300

    feat: cria template com craft js

--- src/features/template/builder/template-builder.tsx
+++ src/features/template/builder/template-builder.tsx
@@ -1,233 +1,475 @@
-import React, { forwardRef } from 'react';
+import React from 'react';
 import type { TemplateBuilderData } from './template-builder-data';
-import { Heading } from '../../../components/heading';
-import { Paragraph } from '../../../components/paragraph';
+import { Editor, Frame, useNode, Element } from '@craftjs/core';
 
 interface TemplateBuilderProps {
   initialData: TemplateBuilderData;
 }
 
-export const TemplateBuilder = forwardRef<object, TemplateBuilderProps>(({ initialData }, ref) => {
+export const TemplateBuilder = ({ initialData }: TemplateBuilderProps) => {
   const { experiences, education, courses, profile, skills, hoobies, languages, links, tools } = initialData;
   const { contact } = profile;
 
   return (
-    <div className="mx-auto flex max-w-[64rem] flex-col px-20 py-56" ref={ref as React.RefObject<HTMLDivElement>}>
-      <header className="sm:px-none flex flex-col items-center gap-24 sm:flex-row">
-        <img className="size-140 rounded-full" src="/foto/foto-pecege.jpeg" alt="" />
-        <div className="flex flex-col text-center sm:text-left">
-          <Heading className="text-40 font-bold lg:text-48 lg:text-nowrap">{profile.name}</Heading>
-          <Paragraph className="font-sans text-28">{profile.role}</Paragraph>
-        </div>
-      </header>
-
-      <main className="mt-48 flex flex-col justify-between gap-2 sm:mt-28 lg:flex-row">
-        <aside className="flex flex-col gap-12 px-4 sm:flex-1/2 lg:pr-48">
-          <section className="flex flex-col gap-12">
-            <Heading className="text-24">Perfil</Heading>
-            <Paragraph>{profile.description}</Paragraph>
-          </section>
-
-          <SectionExperience experiences={experiences} />
-          <SectionEducation education={education} />
-          <SectionCourse courses={courses} />
-        </aside>
-
-        <aside className="mt-24 flex flex-col gap-24 sm:flex-1/6 lg:mt-[0]">
-          <SectionContact contact={contact} />
-          <SectionSkills skills={skills} />
-          <SectionTools tools={tools} />
-          <SectionHoobies hoobies={hoobies} />
-          <SectionLanguages languages={languages} />
-          <SectionLinks links={links} />
-        </aside>
-      </main>
-    </div>
+    <Editor
+      resolver={{
+        Element,
+        Container,
+        Header,
+        Main,
+        AsideLeft,
+        AsideRight,
+        Heading,
+        Paragraph,
+        SectionExperience,
+        SectionEducation,
+        SectionCourse,
+        SectionContact,
+        SectionSkills,
+        SectionTools,
+        SectionHoobies,
+        SectionLanguages,
+        SectionLinks,
+      }}>
+      <Frame>
+        <Element is={Container} canvas>
+          <Element profile={profile} is={Header} />
+          <Element is={Main}>
+            <Element profile={profile} is={AsideLeft} canvas>
+              <Element experiences={experiences} is={SectionExperience} />
+              <Element education={education} is={SectionEducation} />
+              <Element courses={courses} is={SectionCourse} />
+            </Element>
+            <Element is={AsideRight} canvas>
+              <Element contact={contact} is={SectionContact} />
+              <Element skills={skills} is={SectionSkills} />
+              <Element tools={tools} is={SectionTools} />
+              <Element hoobies={hoobies} is={SectionHoobies} />
+              <Element languages={languages} is={SectionLanguages} />
+              <Element links={links} is={SectionLinks} />
+            </Element>
+          </Element>
+        </Element>
+      </Frame>
+    </Editor>
   );
-});
+};
 
 interface SectionLinksProps {
   links: TemplateBuilderData['links'];
 }
 
-const SectionLinks = ({ links }: SectionLinksProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Links</Heading>
-    <ul>
-      {links.map(link => (
-        <li key={link}>{link}</li>
-      ))}
-    </ul>
-  </section>
-);
+const SectionLinks = ({ links }: SectionLinksProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Links</Heading>
+      <ul>
+        {links.map(link => (
+          <li key={link}>{link}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 interface SectionLanguagesProps {
   languages: TemplateBuilderData['languages'];
 }
 
-const SectionLanguages = ({ languages }: SectionLanguagesProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Idioma</Heading>
-    <ul>
-      {languages.map(language => (
-        <li key={language}>{language}</li>
-      ))}
-    </ul>
-  </section>
-);
+const SectionLanguages = ({ languages }: SectionLanguagesProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Idioma</Heading>
+      <ul>
+        {languages.map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 interface SectionHoobiesProps {
   hoobies: TemplateBuilderData['hoobies'];
 }
 
-const SectionHoobies = ({ hoobies }: SectionHoobiesProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Hobbies</Heading>
-    <ul>
-      {hoobies.map(hoobie => (
-        <li key={hoobie}>{hoobie}</li>
-      ))}
-    </ul>
-  </section>
-);
+const SectionHoobies = ({ hoobies }: SectionHoobiesProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Hobbies</Heading>
+      <ul>
+        {hoobies.map(hoobie => (
+          <li key={hoobie}>{hoobie}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 interface SectionToolsProps {
   tools: TemplateBuilderData['tools'];
 }
 
-const SectionTools = ({ tools }: SectionToolsProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Ferramentas e Tecnologias</Heading>
-    <ul>
-      {tools.map((tool, key) => (
-        <li className="inline" key={tool}>
-          {tool}
-          {key < tools.length - 1 && ', '}
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+const SectionTools = ({ tools }: SectionToolsProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Ferramentas e Tecnologias</Heading>
+      <ul>
+        {tools.map((tool, key) => (
+          <li className="inline" key={tool}>
+            {tool}
+            {key < tools.length - 1 && ', '}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 interface SectionSkillsProps {
   skills: TemplateBuilderData['skills'];
 }
 
-const SectionSkills = ({ skills }: SectionSkillsProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Competências</Heading>
-    <ul>
-      {skills.map(skill => (
-        <li key={skill}>{skill}</li>
-      ))}
-    </ul>
-  </section>
-);
+const SectionSkills = ({ skills }: SectionSkillsProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Competências</Heading>
+      <ul>
+        {skills.map(skill => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 interface SectionContactProps {
   contact: TemplateBuilderData['profile']['contact'];
 }
 
-const SectionContact = ({ contact }: SectionContactProps) => (
-  <section className="flex flex-col">
-    <Heading className="mb-12 text-24">Contato</Heading>
+const SectionContact = ({ contact }: SectionContactProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
 
-    <ul>
-      <li>{contact.address}</li>
-      <li>{`${contact.postalCode}, ${contact.neighborhood} - ${contact.city} - ${contact.state}`}</li>
-      <li>
-        {contact.phones.map((phone, key) => (
-          <span key={phone}>
-            {phone}
-            {key < contact.phones.length - 1 && ' / '}
-          </span>
-        ))}
-      </li>
-      <li>{`${contact.birth.date} - ${contact.birth.maritalStatus} - ${contact.birth.age} anos`}</li>
-      <li>Email: {contact.emails.map(email => email)}</li>
-      <li>Nacionalidade: {contact.nationality}</li>
-      <li>Naturalidade: {contact.birthplace}</li>
-      <li>Habilitação: {contact.drivingLicense}</li>
-    </ul>
-  </section>
-);
+  return (
+    <section
+      className="flex flex-col"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="mb-12 text-24">Contato</Heading>
 
-TemplateBuilder.displayName = 'TemplateBuilder';
-
-interface SectionExperienceProps {
-  experiences: TemplateBuilderData['experiences'];
-}
-
-const SectionExperience = ({ experiences }: SectionExperienceProps) => (
-  <section className="mt-12 flex flex-col gap-12">
-    <Heading className="text-24">Experiência Profissional</Heading>
-    {experiences.map(exp => (
-      <React.Fragment key={exp.title}>
-        <div>
-          <strong>
-            {`${exp.title} - ${exp.location} - `}
-            <span>{exp.duration},</span>
-          </strong>
-          <Paragraph className="text-14 italic">{`${exp.company} - (${exp.mode})`}</Paragraph>
-        </div>
-
-        <ul className="list-disc pl-24">
-          {exp.responsibilities.map(resp => (
-            <li key={resp}>{resp}</li>
+      <ul>
+        <li>{contact.address}</li>
+        <li>{`${contact.postalCode}, ${contact.neighborhood} - ${contact.city} - ${contact.state}`}</li>
+        <li>
+          {contact.phones.map((phone, key) => (
+            <span key={phone}>
+              {phone}
+              {key < contact.phones.length - 1 && ' / '}
+            </span>
           ))}
-        </ul>
-
-        <p className="mt-12 break-words">
-          <span className="mr-2">
-            <strong>Competências:</strong>
-          </span>
-
-          {exp.skills.map(skill => (
-            <span key={skill}>{skill},</span>
-          ))}
-        </p>
-      </React.Fragment>
-    ))}
-  </section>
-);
+        </li>
+        <li>{`${contact.birth.date} - ${contact.birth.maritalStatus} - ${contact.birth.age} anos`}</li>
+        <li>Email: {contact.emails.map(email => email)}</li>
+        <li>Nacionalidade: {contact.nationality}</li>
+        <li>Naturalidade: {contact.birthplace}</li>
+        <li>Habilitação: {contact.drivingLicense}</li>
+      </ul>
+    </section>
+  );
+};
 
 interface SectionEducationProps {
   education: TemplateBuilderData['education'];
 }
 
-const SectionEducation = ({ education }: SectionEducationProps) => (
-  <section className="mt-12 flex flex-col gap-12">
-    <Heading className="text-24">Formação</Heading>
-    {education.map(educ => (
-      <Paragraph key={educ.course}>
-        <strong className="block">
-          {educ.course} - {educ.start} - {educ.end}, {educ.city}
-        </strong>
-        <span className="text-14 italic">{educ.institution}</span>
-      </Paragraph>
-    ))}
-  </section>
-);
+const SectionEducation = ({ education }: SectionEducationProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="mt-12 flex flex-col gap-12"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="text-24">Formação</Heading>
+      {education.map(educ => (
+        <Paragraph key={educ.course}>
+          <strong className="block">
+            {educ.course} - {educ.start} - {educ.end}, {educ.city}
+          </strong>
+          <span className="text-14 italic">{educ.institution}</span>
+        </Paragraph>
+      ))}
+    </section>
+  );
+};
 
 interface SectionCourseProps {
   courses: TemplateBuilderData['courses'];
 }
 
-const SectionCourse = ({ courses }: SectionCourseProps) => (
-  <section className="mt-12 flex flex-col gap-12">
-    <Heading className="text-24">Cursos</Heading>
-    {courses.map(course => (
-      <React.Fragment key={course.title}>
-        <strong>
-          Formação: Desenvolva aplicações Web Acessíveis -{' '}
-          <span className="font-medium italic">Alura - Setembro, 2023</span>
-        </strong>
-        <ul className="list-disc pl-24">
-          {course.modules.map(module => (
-            <li key={module}>{module}</li>
-          ))}
-        </ul>
-      </React.Fragment>
-    ))}
-  </section>
-);
+const SectionCourse = ({ courses }: SectionCourseProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="mt-12 flex flex-col gap-12"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="text-24">Cursos</Heading>
+      {courses.map(course => (
+        <React.Fragment key={course.title}>
+          <strong>
+            Formação: Desenvolva aplicações Web Acessíveis -{' '}
+            <span className="font-medium italic">Alura - Setembro, 2023</span>
+          </strong>
+          <ul className="list-disc pl-24">
+            {course.modules.map(module => (
+              <li key={module}>{module}</li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ))}
+    </section>
+  );
+};
+
+interface HeaderProps {
+  profile: TemplateBuilderData['profile'];
+}
+
+const Header = ({ profile }: HeaderProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <header
+      className="sm:px-none flex flex-col items-center gap-24 sm:flex-row"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <img className="size-140 rounded-full" src="/foto/foto-pecege.jpeg" alt="" />
+      <div className="flex flex-col text-center sm:text-left">
+        <Heading className="text-40 font-bold lg:text-48 lg:text-nowrap">{profile.name}</Heading>
+        <Paragraph className="font-sans text-28">{profile.role}</Paragraph>
+      </div>
+    </header>
+  );
+};
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+  return (
+    <div
+      className="mx-auto flex max-w-[64rem] flex-col px-20 py-56"
+      ref={(ref: HTMLDivElement) => {
+        connect(drag(ref));
+      }}>
+      {children}
+    </div>
+  );
+};
+
+interface MainProps {
+  children: React.ReactNode;
+}
+
+const Main = ({ children }: MainProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+  return (
+    <main
+      className="mt-48 flex flex-col justify-between gap-2 sm:mt-28 lg:flex-row"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      {children}
+    </main>
+  );
+};
+
+interface AsideLeftProps {
+  children: React.ReactNode;
+  profile: TemplateBuilderData['profile'];
+}
+
+const AsideLeft = ({ children, profile }: AsideLeftProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <aside
+      className="flex flex-col gap-12 px-4 sm:flex-1/2 lg:pr-48"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <section className="flex flex-col gap-12">
+        <Heading className="text-24">Perfil</Heading>
+        <Paragraph>{profile.description}</Paragraph>
+      </section>
+
+      {children}
+    </aside>
+  );
+};
+
+interface AsideRightProps {
+  children: React.ReactNode;
+}
+
+const AsideRight = ({ children }: AsideRightProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <aside
+      className="mt-24 flex flex-col gap-24 sm:flex-1/6 lg:mt-[0]"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      {children}
+    </aside>
+  );
+};
+
+interface ParagraphProps
+  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement> {}
+
+export const Paragraph = ({ children, ...props }: ParagraphProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <p
+      {...props}
+      ref={(ref: HTMLParagraphElement) => {
+        connect(drag(ref));
+      }}>
+      {children}
+    </p>
+  );
+};
+
+interface HeadingProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement> {}
+
+export const Heading = ({ children, ...props }: HeadingProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <h1
+      {...props}
+      ref={(ref: HTMLHeadingElement) => {
+        connect(drag(ref));
+      }}>
+      {children}
+    </h1>
+  );
+};
+
+interface SectionExperienceProps {
+  experiences: TemplateBuilderData['experiences'];
+}
+
+const SectionExperience = ({ experiences }: SectionExperienceProps) => {
+  const {
+    connectors: { connect, drag },
+  } = useNode();
+
+  return (
+    <section
+      className="mt-12 flex flex-col gap-12"
+      ref={(ref: HTMLElement) => {
+        connect(drag(ref));
+      }}>
+      <Heading className="text-24">Experiência Profissional</Heading>
+      {experiences.map(exp => (
+        <React.Fragment key={exp.title}>
+          <div>
+            <strong>
+              {`${exp.title} - ${exp.location} - `}
+              <span>{exp.duration},</span>
+            </strong>
+            <Paragraph className="text-14 italic">{`${exp.company} - (${exp.mode})`}</Paragraph>
+          </div>
+
+          <ul className="list-disc pl-24">
+            {exp.responsibilities.map(resp => (
+              <li key={resp}>{resp}</li>
+            ))}
+          </ul>
+
+          <p className="mt-12 break-words">
+            <span className="mr-2">
+              <strong>Competências:</strong>
+            </span>
+
+            {exp.skills.map(skill => (
+              <span key={skill}>{skill},</span>
+            ))}
+          </p>
+        </React.Fragment>
+      ))}
+    </section>
+  );
+};
--- src/features/template/editor/editor.tsx
+++ src/features/template/editor/editor.tsx
@@ -1,42 +1,11 @@
-import { fakeTemplateApiResponse } from '../builder/template-builder-data';
-
-import { Editor, Frame, Element } from '@craftjs/core';
+import { TemplateBuilder } from '../builder/template-builder';
+import { templateBuilderData } from '../builder/template-builder-data';
 
 export const TemplateEditor = () => {
   return (
     <div className="flex flex-col">
-      <Editor>
-        <TemplateEditorContent />
-      </Editor>
-    </div>
-  );
-};
-
-const TemplateEditorContent = () => {
-  // TODO: pegar o elemento correto para gerar o JSON.
-  /*  const handleSave = () => {
-    if (templateRef?.current) {
-      const json = domToJSON(templateRef.current);
-      console.log(json.children);
-    }
-  }; */
-
-  const handleEdit = () => {
-    // setIsEdit();
-    // if (isEdit) handleSave();
-  };
-
-  //  const handleDownloadPDF = () => console.log('TODO: download PDF');
-
-  console.log(Array.prototype.flat.call(fakeTemplateApiResponse.page.children));
-
-  return (
-    <>
       <header className="container mx-auto flex h-80 w-full items-end justify-center gap-12 sm:h-140 sm:justify-end">
-        {/* TODO: instalar tailwind merge para classes */}
-        <button
-          onClick={handleEdit}
-          className={`h-48 cursor-pointer rounded-2xl ${/*isEdit ? 'bg-green-200' :*/ 'bg-blue-400'} px-20`}>
+        <button className={`h-48 cursor-pointer rounded-2xl ${/*isEdit ? 'bg-green-200' :*/ 'bg-blue-400'} px-20`}>
           {/*isEdit ? 'Salvar' : 'Editar'*/} SALVAR/EDITAR
         </button>
 
@@ -47,21 +16,7 @@ const TemplateEditorContent = () => {
         )*/}
       </header>
 
-      <Frame>
-        <Element></Element>
-      </Frame>
-    </>
+      <TemplateBuilder initialData={templateBuilderData} />
+    </div>
   );
 };
-
-/* XXX: funcionamento
- * componentes abstratos (craftjs)
- * <CraftWapper><Button/></CraftWapper>
- * monta o template e salva o JSON (serialize)
- * o json é salvo no banco, e o template busca na api e carrega como foi salvo.
- * -------
- * É uma aplicação de curriculo, portanto os eventListeners e helpers são abstratos e reaproveitados.
- * cada modelo precisará inicialmente ser montado manualmente para que o usuario possa escolhar e customizar.
- * esse processo manual poderá ser automatizado através de uma interface 'admin' drag and drop para montar novos templates.
- * os componentes 'arrastaveis' sáo os abstratos
- */
